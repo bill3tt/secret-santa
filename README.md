2340d9aefad4dfbdb9acf00b95e63b432c68a455
Ian Billett <ibillett@redhat.com>
2021-11-14 17:55:18 +0000

    adds script & README
    Signed-off-by: Ian Billett <ibillett@redhat.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Secret Santa
+
+This is a super simple python script that prints out this year's secret santa picks.
---
+++ secret-santa.py
@@ -0,0 +1,32 @@
+import itertools
+import random
+
+people = ["Ian", "Joel", "Robbie", "Connor", "Jason", "Jimmy", "Gareth"]
+
+past_years = [
+    # 2020
+    ("Ian", "Jason"),
+    ("Jimmy", "Ian"),
+    ("Connor", "Robbie"),
+    ("Jason", "Gareth"),
+    ("Gareth", "Joel"),
+    ("Joel", "Jimmy"),
+    ("Robbie", "Connor"),
+]
+
+# Generate every pair-length permutation of the original list of people.
+pick_pool = itertools.permutations(people, 2)
+# Regenerate the pick_pool, exlcuding all previous year's picks.
+pick_pool = [p for p in itertools.permutations(people, 2) if p not in past_years]
+
+this_years_picks = []
+
+while pick_pool:
+    pick = random.choice(pick_pool)
+    this_years_picks.append(pick)
+    buyer, _ = pick
+    # Regenerate the pick pool, excluding the current buyer.
+    pick_pool = [pick for pick in pick_pool if pick[0] != buyer]
+
+print(this_years_picks)
+
